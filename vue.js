0f8e66585d91c37ca1456d16b593bda4f38f4fcd
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-11-09 17:18:21 +0100

    fine

--- vue.js
+++ vue.js
@@ -10,38 +10,76 @@ const app = new Vue ({
 
         urlMail :"https://flynn.boolean.careers/exercises/api/random/mail",
         userMail: [],
+        pendigList:0,
+        list:0,
 
     },
 
     methods:{
-        generateMailServer(){
-            for (let x = 0; x < 10; x++) {
-                // const element = array[x];
-                axios.get(this.urlMail).then((element) =>{
-                    console.log(element.data)
         
-                    let rispostaServer = element.data
-    
-                    this.userMail.push(rispostaServer.response);
-                    console.log(this.userMail)
+    //     generateMailServer(){
+    //         // let tempMail = []
+    //         this.pendigList = 10 
+    //         for (let x = 0; x < 10; x++) {
+    //             // const element = array[x];
+    //             axios.get(this.urlMail).then((element) =>{
+    //                 console.log(element.data)
+    //                 // this.pendigList--
+        
+    //                 let rispostaServer = element.data
+    //                 this.userMail.push(rispostaServer.response)
+    //                 // tempMail.push(rispostaServer.response);
+    //                 // if (this.pendigList === 0) {
+    //                 //     this.userMail = tempMail
+    //                 // }
+                   
+    //                 console.log(this.userMail)
 
-                })
+    //             })
+
+             
+    //     }
+
+        
+
+    // },
+
+    generatMail(){
+        let tempMail = []
+        this.list  = 10
+
+        for (let x = 0; x < 10; x++) {
+            axios.get(this.urlMail).then((elMail)=>{
+                this.list--
+
+
+                let server = elMail.data.response
+
+                tempMail.push(server)
+
+                if (this.list === 0) {
+                    this.userMail = tempMail
+                    
+                    console.log(tempMail)
+                }
+                
+
+            })
+            
         }
+    }
 
 
-    },
+    // mounted(){
 
+    //     // vado a prendere il api di Boolean per generare randomicamente le mail io in questo caso la vado a mettere nel data per poterla riutilizzare dove vogli
 
-    mounted(){
+    //     // let url = "https://flynn.boolean.careers/exercises/api/random/mail"
 
-        // vado a prendere il api di Boolean per generare randomicamente le mail io in questo caso la vado a mettere nel data per poterla riutilizzare dove vogli
-
-        // let url = "https://flynn.boolean.careers/exercises/api/random/mail"
-
-        // vado a caricare in rete il mio api
+    //     // vado a caricare in rete il mio api
 
        
-        }
+    //     }
  
 
     }
